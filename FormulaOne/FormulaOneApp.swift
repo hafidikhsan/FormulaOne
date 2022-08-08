1d4613c7f672b3725edfa1a64e7531ea515922ca
Hafid Ikhsan Arifin <harifin315@gmail.com>
2022-08-08 20:02:44 +0700

    Initial Commit

---
+++ FormulaOne/FormulaOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaOneApp.swift
+//  FormulaOne
+//
+//  Created by Hafid Ikhsan Arifin on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct FormulaOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
