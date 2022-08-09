4ca56175ba2c2ea74ccec2509728bcce1e1c1f42
Hafid Ikhsan Arifin <harifin315@gmail.com>
2022-08-09 11:47:15 +0700

    Add AsyncImage

--- FormulaOne/ContentView.swift
+++ FormulaOne/ContentView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct ContentView: View {
     
     @State var results = [Teams]()
+    @State var image = ""
     
     var body: some View {
+        if (image != "") {
+            AsyncImage(url: URL(string: image))
+        }
         Button("Load") {
             let loadData = LoadData()
             loadData.endPoint = "rankings/teams?season=2022"
             loadData.LoadDataNow{
                 (teams) in self.results = teams
+                self.image = teams[0].team.logo
                 print(results)
             }
         }
